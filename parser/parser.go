c351eb2516f32c9f279a3be23dcc6d18b1d17f35
Sheen An <sheen.an.goh@gmail.com>
2024-08-22 16:11:24 +0800

    feat: support if else expression

--- parser/parser.go
+++ parser/parser.go
@@ -145,6 +145,15 @@ func (p *Parser) parseIfExpression() ast.Expression {
 		return nil
 	}
 	expression.Consequence = p.parseBlockStatement()
+
+	if p.peekTokenIs(token.ELSE) {
+		p.nextToken()
+		if !p.expectPeek(token.LBRACE) {
+			return nil
+		}
+		expression.Alternative = p.parseBlockStatement()
+	}
+
 	return expression
 }
 
