38542e625d6923f06a79aa235d53a3ca678b774b
Sheen An <sheen.an.goh@gmail.com>
2024-08-23 13:13:18 +0800

    parse expression in return and let statements

--- parser/parser.go
+++ parser/parser.go
@@ -317,10 +317,14 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 		return nil
 	}
 
-	//TODO: We are skipping the expressions until we encounter a SEMICOLON
-	for !p.curTokenIs(token.SEMICOLON) {
+	p.nextToken()
+
+	stmt.Value = p.parseExpression(LOWEST)
+
+	for p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
+
 	return stmt
 }
 
@@ -328,7 +332,8 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 	stmt := &ast.ReturnStatement{Token: p.curToken}
 	p.nextToken()
 
-	//TODO: We are skipping the expressions until we encounter a SEMICOLON
+	stmt.ReturnValue = p.parseExpression(LOWEST)
+
 	for !p.curTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
