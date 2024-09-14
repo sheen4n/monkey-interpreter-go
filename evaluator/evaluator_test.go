4a17db7c7946d505afd36668c304fca1db4aef6c
Sheen An <sheen.an.goh@gmail.com>
2024-09-14 20:49:34 +0800

    feat: evaluate nested conditions

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -162,7 +162,17 @@ func TestReturnStatements(t *testing.T) {
 		{"return 10; 9;", 10},
 		{"return 2 * 5; 9;", 10},
 		{"9; return 2 * 5; 9;", 10},
+		{`if (10 > 2) {return 50;}`, 50},
+		{
+			`
+			if (10 > 1) {
+				if (10 > 1) {
+					return 10;
+				}
+				return 1;
+			}`, 10},
 	}
+
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
 		testIntegerObject(t, evaluated, tt.expected)
