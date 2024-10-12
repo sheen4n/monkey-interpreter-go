43b90d73536c509e87d8038a15f8e2bd6941b3c0
Sheen An Goh <sheen.an.goh@gmail.com>
2024-10-12 17:06:45 +0800

    implement builtin method: len

--- object/object.go
+++ object/object.go
@@ -17,6 +17,7 @@ const (
 	ERROR_OBJ        = "ERROR"
 	FUNCTION_OBJ     = "FUNCTION"
 	STRING_OBJ       = "STRING"
+	BUILTIN_OBJ      = "BUILTIN"
 )
 
 type Object interface {
@@ -78,3 +79,9 @@ type String struct{ Value string }
 
 func (s *String) Type() ObjectType { return STRING_OBJ }
 func (s *String) Inspect() string  { return s.Value }
+
+type BuiltinFunction func(args ...Object) Object
+type Builtin struct{ Fn BuiltinFunction }
+
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
+func (b *Builtin) Inspect() string  { return "builtin function" }
